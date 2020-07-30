b0b67cded1d9a185c05887b7fc5752a7bf1d24e6
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2020-07-30 17:18:32 -0300

    generics generator files

--- bin/add_watermark_pdf.py
+++ bin/add_watermark_pdf.py
@@ -1,4 +1,4 @@
-# python ~/dotfiles/bin/add_watermark_pdf.py -i /home/tiagoaguiar/Downloads/ebookxxx.pdf -p "email@hotmail.com" -n "FulanoDaSilva" -o /home/tiagoaguiar/Downloads/output.pdf 
+# python ~/dotfiles/bin/add_watermark_pdf.py -i /home/tiagoaguiar/Downloads/ebookxxx.pdf -p "email@hotmail.com" -n "FulanoDaSilva" -o /home/tiagoaguiar/Downloads/output.pdf
 # Pillow==6.2.1
 # PyPDF2==1.26.0
 # reportlab==3.5.32
@@ -16,8 +16,8 @@ def set_watermark(input_pdf, user_password, user_name, output_pdf):
 
     # create a new PDF with Reportlab
     can = canvas.Canvas(packet, pagesize=letter)
-    can.setFillColorRGB(1,0,0)
-    can.drawString(20, 20, "{} - {}".format(user_name, user_password))
+    can.setFillColorRGB(1, 0, 0)
+    can.drawString(20, 20, user_name)
     can.save()
 
     # move to the beginning of the StringIO buffer
@@ -44,17 +44,45 @@ def set_watermark(input_pdf, user_password, user_name, output_pdf):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_pdf', required=True,
-                        help='Input pdf file')
-    parser.add_argument('-p', '--user_password', required=True,
-                        help='output CSV file')
-    parser.add_argument('-n', '--user_name', required=True,
-                        help='output CSV file')
-    parser.add_argument('-o', '--output_pdf', required=True,
-                        help='output CSV file')
-    args = parser.parse_args()
-    set_watermark(args.input_pdf, args.user_password, args.user_name, args.output_pdf)
+    #parser = argparse.ArgumentParser()
+    # parser.add_argument('-i', '--input_pdf', required=True,
+    #                    help='Input pdf file')
+    # parser.add_argument('-p', '--user_password', required=True,
+    #                    help='output CSV file')
+    # parser.add_argument('-n', '--user_name', required=True,
+    #                    help='output CSV file')
+    # parser.add_argument('-o', '--output_pdf', required=True,
+    #                    help='output CSV file')
+    #args = parser.parse_args()
+
+    source = {
+        "origins": [
+            "ebooktaticasdemonetizacao.pdf",
+            "eBookLancamentodeaplicativos.pdf",
+            "ebookmarketingdeaplicativos.pdf",
+        ],
+        "values": [
+            {
+                "password": "1234",
+                "name": "FulanoDaSilva",
+                "address": "Rua A"
+            },
+        ]
+    }
+
+    values = source["values"]
+    origins = source["origins"]
+
+    for item in values:
+        for origin in origins:
+            print(origin + "para " + item['name'])
+            set_watermark("{0}{1}".format("/Volumes/GoogleDrive/My Drive/AT Digital/eBooks/", origin),
+                          item['password'],
+                          "Licenciado: {0}, CPF:{1},{2}".format(item['name'],
+                                                                item['password'],
+                                                                item['address']),
+                          "{0}-{1}".format(item["name"], origin))
+
 
 if __name__ == "__main__":
     main()
