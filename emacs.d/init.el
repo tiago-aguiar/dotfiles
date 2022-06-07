d7bb4b88a2c96f9fad751afbc4a93c97267cdf5b
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2022-06-06 22:43:05 -0300

    macos meta and fonts

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,14 +21,19 @@
 (scroll-bar-mode -1)               ;; disable scroll bar
 (tool-bar-mode -1)                 ;; disable toolbar 
 (menu-bar-mode -1)                 ;; disable menubar
-(setq visible-bell t)              ;; flash screen hit end line
 (column-number-mode t)             ;; enable line and column at modeline (bar)
 (show-paren-mode t)                ;; enable pair brackets/parentheses
 (global-hl-line-mode -1)           ;; disable highlight current line
 
+(when (not is-macos)
+  setq visible-bell t) ;; flash screen hit end line
 
 ;; change font (height = percent)
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
+(when (member "Fira Code Retina" (font-family-list)) 
+ (set-face-attribute 'default nil :font "Fira Code Retina" :height 120))
+
+(when (member "Inconsolata for Powerline" (font-family-list)) 
+ (set-face-attribute 'default nil :font "Inconsolata for Powerline" :height 180))
 
 ;; change yes-no to y-n
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -38,18 +43,6 @@
 (add-to-list 'custom-theme-load-path (file-name-as-directory "."))
 (load-theme 'taguiar t t)
 (enable-theme 'taguiar)
-
-;; make ESC quit
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-(define-key global-map "\es" 'save-buffer)
-(define-key global-map "\ek" 'kill-current-buffer)
-
-(define-key global-map "\eb" 'ivy-switch-buffer)
-(define-key global-map "\eB" 'ivy-switch-buffer-other-window)
-
-(define-key global-map "\ef" 'find-file)
-(define-key global-map "\eF" 'find-file-other-window)
 
 
 ;; initialize package sources
@@ -105,3 +98,27 @@
   :diminish wich-key-mode
   :config
   (setq which-key-idle-delay 0.3))
+
+;; tuning for ivy
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)))
+
+;; documentation for emacs commands
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+
+(setq mac-command-modifier 'meta) ;; switch Option to Command key (meta)
+
+;; make ESC quit
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(define-key global-map "\es" 'save-buffer)
+(define-key global-map "\ek" 'kill-current-buffer)
+
+(define-key global-map "\eb" 'counsel-ibuffer)
+(define-key global-map "\eB" 'ivy-switch-buffer-other-window)
+
+(define-key global-map "\ef" 'find-file)
+(define-key global-map "\eF" 'find-file-other-window)
