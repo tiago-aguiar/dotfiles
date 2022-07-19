39a821a2d9f81b92a41346265c5945673aebe1db
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2022-07-18 21:22:35 -0300

    search for build script recursive

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -322,11 +322,31 @@ fixme-modes)
   (compile taguiar-launchscript)
   (other-window 1))
 
+(setq compilation-directory-locked nil)
+
+(defun find-project-directory-recursive ()
+  "Recursively search for a makefile."
+  (interactive)
+  (if (file-exists-p taguiar-makescript) t
+      (cd "../")
+      (find-project-directory-recursive)))
+
+(defun find-project-directory ()
+  "Find the project directory."
+  (interactive)
+  (setq find-project-from-directory default-directory)
+  (switch-to-buffer-other-window "*compilation*")
+  (if compilation-directory-locked (cd last-compilation-directory)
+  (cd find-project-from-directory)
+  (find-project-directory-recursive)
+  (setq last-compilation-directory default-directory)))
+
 (defun make-without-asking ()
   "Make the current build."
   (interactive)
-  (compile taguiar-makescript)
+  (if (find-project-directory) (compile taguiar-makescript))
   (other-window 1))
+
 
 (define-key global-map "\el" 'evil-window-right)
 (define-key global-map "\eh" 'evil-window-left)
