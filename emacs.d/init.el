86015a3a54c2a66df38a60bde28e47948ee9ff34
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2022-09-10 17:18:44 -0300

    win32 with liberation

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -88,20 +88,21 @@
   (message "Load Inconsolata")
   (set-face-attribute 'default nil :font "Inconsolata for Powerline" :height 166))
 
-(when (member "Fira Code Retina" (font-family-list))
-  (message "Load Fira Code")
-  (set-face-attribute 'default nil :font "Fira Code Retina" :height 120))
-
 (when is-macos
   (when (member "Liberation Mono for Powerline" (font-family-list)) 
     (message "Load Liberation Mono")
     (set-face-attribute 'default nil :font "Liberation Mono for Powerline" :height 140)))
 
 (when is-win32
-  (when (member "Lucida Console" (font-family-list))
-    (message "Load Lucida Console")
-    (setq-default line-spacing 1)
-    (set-face-attribute 'default nil :font "Lucida Console" :height 120)))
+  (when (member "Liberation Mono" (font-family-list)) 
+    (message "Load Liberation Mono")
+    (set-face-attribute 'default nil :font "Liberation Mono" :height 130)))
+
+;; (when is-win32
+;;   (when (member "Lucida Console" (font-family-list))
+;;     (message "Load Lucida Console")
+;;     (setq-default line-spacing 1)
+;;     (set-face-attribute 'default nil :font "Lucida Console" :height 120)))
 
 ;; ;; change yes-no to y-n
 (defalias 'yes-or-no-p 'y-or-n-p)
