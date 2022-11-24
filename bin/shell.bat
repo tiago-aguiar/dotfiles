a2ccd7953caf7b48b5e1a2664698d932bcdcd3ec
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2022-11-23 23:28:17 -0300

    java snippets

--- bin/shell.bat
+++ bin/shell.bat
@@ -5,11 +5,12 @@ call "%ConEmuDir%"\..\init.bat"
 set path=e:\Tiago\bin;%path%
 set PATH=c:\Program Files (x86)\GnuWin32\bin;%PATH%
 set PATH=c:\MinGW\bin;%PATH%
+set PATH=c:\Java\jdk-19.0.1\bin;%PATH%
 set PATH=e:\Android\Sdk\emulator;%PATH%
 set PATH=e:\Android\Sdk\platform-tools;%PATH%
 set PATH=e:\Tiago\programming\kotlin-language-server\server\build\install\server\bin;%PATH%
 set PKG_CONFIG_PATH=c:\PkgConfigs
-set JAVA_HOME="e:\Android Studio\jre"
+set JAVA_HOME="C:\Java\jdk-19.0.1"
 
 alias l=ls -al --color $*
 alias gs=git status
