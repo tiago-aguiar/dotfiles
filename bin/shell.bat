ce06c0490dde928749d0a9bc5bf7156f571c30f4
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2023-07-11 19:07:59 -0300

    fix conEmu

--- bin/shell.bat
+++ bin/shell.bat
@@ -1,6 +1,6 @@
 @echo off
 call "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-call "%ConEmuDir%"\..\init.bat"
+REM call "%ConEmuDir%"\..\init.bat"
 
 set path=e:\Tiago\bin;%path%
 set PATH=c:\Program Files (x86)\GnuWin32\bin;%PATH%
